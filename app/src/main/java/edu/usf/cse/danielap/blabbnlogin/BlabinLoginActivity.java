096106470999ee7a4fbe2e674f9e7cfd91bedb03
Adriano Triana <Adriano Triana>
2017-03-07 12:44:45 -0500

    ughhhhhh

--- app/src/main/java/edu/usf/cse/danielap/blabbnlogin/BlabinLoginActivity.java
+++ app/src/main/java/edu/usf/cse/danielap/blabbnlogin/BlabinLoginActivity.java
@@ -42,6 +42,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
+
 public class BlabinLoginActivity extends AppCompatActivity {
 
 
