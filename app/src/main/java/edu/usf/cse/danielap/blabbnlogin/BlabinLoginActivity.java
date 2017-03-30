07a84e2556f653a140a64a1edd666a65b5868c9e
Adriano Triana <Adriano Triana>
2017-03-30 15:34:47 -0400

    Completed functionality for login authentication and added basis for adding a whale in the Whale list screen activity.

--- app/src/main/java/edu/usf/cse/danielap/blabbnlogin/BlabinLoginActivity.java
+++ app/src/main/java/edu/usf/cse/danielap/blabbnlogin/BlabinLoginActivity.java
@@ -1,50 +1,40 @@
 package edu.usf.cse.danielap.blabbnlogin;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.database.DatabaseUtils;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.ContentResolver;
-import android.content.CursorLoader;
-import android.content.Loader;
 import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build.VERSION;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 import cse.usf.edu.android.db.BlabbinDBManager;
 
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
 
 public class BlabinLoginActivity extends AppCompatActivity {
+
+    //Firebase Authentication Code.
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    public static final String TAG = "Blabbin Login";
 
 
 
@@ -61,6 +51,27 @@ public class BlabinLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blabin_login);
 
+
+        //Firebase authentication Code
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
         // Get Username and Password field views
         mUserNameView = (AutoCompleteTextView) findViewById(R.id.email);
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -69,12 +80,13 @@ public class BlabinLoginActivity extends AppCompatActivity {
         myBlabbinDBManager = new BlabbinDBManager(this);
         myBlabbinDBManager.open();
 
+
         // Create a User
         Button createUser = (Button) findViewById(R.id.create_user_button);
         createUser.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                newUser();
+                createAccount();
             }
         });
 
@@ -83,7 +95,7 @@ public class BlabinLoginActivity extends AppCompatActivity {
         login.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                userLogin();
+                signIn();
             }
         });
 
@@ -91,21 +103,113 @@ public class BlabinLoginActivity extends AppCompatActivity {
         String fontHobo = "fonts/Hobo Std Medium.ttf";
         String fontHelvetica = "fonts/HelveticaNeue.ttf";
 
-//        // text view label
-//        TextView txtLoginbut = (TextView) findViewById(R.id.email_sign_in_button);
-//        TextView txtCreateUser = (TextView) findViewById(R.id.create_user_button);
-//        TextView txtemail = (TextView) findViewById(R.id.email);
-//        TextView txtpassword = (TextView) findViewById(R.id.password);
-
         // Loading Font Face
         Typeface tfHobo = Typeface.createFromAsset(getAssets(), fontHobo);
         Typeface tfHelvetica = Typeface.createFromAsset(getAssets(), fontHelvetica);
 
-//        // Applying font
-//        txtLoginbut.setTypeface(tfHobo);
-//        txtemail.setTypeface(tfHelvetica);
-//        txtpassword.setTypeface(tfHelvetica);
-//        txtCreateUser.setTypeface(tfHobo);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    private void createAccount(){
+
+        String email = mUserNameView.getText().toString();
+        String pw = mPasswordView.getText().toString();
+
+
+        mAuth.createUserWithEmailAndPassword(email, pw)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(BlabinLoginActivity.this, "Authentication failed. Try again.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            Intent i = new Intent(BlabinLoginActivity.this, BlabinLoginActivity.class);
+                            startActivity(i);
+
+                            // close this activity
+                            finish();
+                        }
+                        else {
+                            Intent i = new Intent(BlabinLoginActivity.this, BlabbinChooseWhaleActivity.class);
+                            startActivity(i);
+
+                            // close this activity
+                            finish();
+                        }
+
+                        // ...
+                    }
+                });
+
+//        Intent i = new Intent(BlabinLoginActivity.this, BlabbinChooseWhaleActivity.class);
+//        startActivity(i);
+//
+//        // close this activity
+//        finish();
+
+    }
+
+
+    private void signIn(){
+
+
+        String email = mUserNameView.getText().toString();
+        String pw = mPasswordView.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, pw)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(BlabinLoginActivity.this, "Authentication failed. Try again.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            Intent i = new Intent(BlabinLoginActivity.this, BlabinLoginActivity.class);
+                            startActivity(i);
+                            finish();
+
+                        }
+                        else{
+                            Intent i = new Intent(BlabinLoginActivity.this, WhaleListScreen.class);
+                            startActivity(i);
+                            finish();
+                        }
+
+                        // ...
+                    }
+                });
+
+//        Intent i = new Intent(BlabinLoginActivity.this, WhaleListScreen.class);
+//        startActivity(i);
+//        finish();
+
 
     }
 
--- app/src/main/java/edu/usf/cse/danielap/blabbnlogin/WhaleListScreen.java
+++ app/src/main/java/edu/usf/cse/danielap/blabbnlogin/WhaleListScreen.java
@@ -9,33 +9,74 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class WhaleListScreen extends AppCompatActivity{
+
+    TextView whaleName;
+    ImageButton addWhale;
+    EditText inputWhaleName;
+    Button submit;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whale_list_screen);
-        defineButtonsChooseWhale();
-    }
 
-    public void defineButtonsChooseWhale() {
-        findViewById(R.id.buttonInitialWhale).setOnClickListener(buttonClickListener);
-    }
+        whaleName = (TextView) findViewById(R.id.text_add_whale);
+        addWhale = (ImageButton) findViewById(R.id.buttonAddWhale);
+        inputWhaleName = (EditText) findViewById(R.id.NameWhale);
+        submit = (Button) findViewById(R.id.SubmitWhaleName);
 
-    final Context context = this;
-    Intent intent;
+        addWhale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addWhale.setVisibility(view.INVISIBLE);
+                whaleName.setVisibility(view.INVISIBLE);
 
-    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.buttonInitialWhale:
-                    intent = new Intent(context, BlabbinWhaleActivity.class);
-                    startActivity(intent);
-                    break;
+                inputWhaleName.setVisibility(view.VISIBLE);
+                submit.setVisibility(view.VISIBLE);
+
+
+
             }
-        }
-    };
+        });
+
+
+
+
+
+    }
+
+
+
+//    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//
+//                if (view.getId() == R.id.buttonAddWhale) {
+//                    View createWhale = WhaleListScreen.this.findViewById(R.id.text_add_whale);
+//                    createWhale.setVisibility(View.INVISIBLE);
+//
+//                    View whaleName = WhaleListScreen.this.findViewById(R.id.NameWhale);
+//                    whaleName.setVisibility(View.VISIBLE);
+//
+//
+//                }
+//
+//
+//            switch (view.getId()) {
+//                case R.id.buttonInitialWhale:
+////                    intent = new Intent(context, BlabbinWhaleActivity.class);
+////                    startActivity(intent);
+//                    break;
+//            }
+//        }
+//    };
 
 }
