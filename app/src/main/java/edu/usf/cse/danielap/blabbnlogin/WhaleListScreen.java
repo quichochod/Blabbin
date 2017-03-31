1749dddf90615b18c450b454a92c059e4f6e5ae4
quichochod <quichochod@mail.usf.edu>
2017-03-31 00:17:45 -0400

    Moved refresh button listener to onCreate method

--- app/src/main/java/edu/usf/cse/danielap/blabbnlogin/WhaleListScreen.java
+++ app/src/main/java/edu/usf/cse/danielap/blabbnlogin/WhaleListScreen.java
@@ -109,6 +109,13 @@ public class WhaleListScreen extends AppCompatActivity  implements GoogleApiClie
             }
         });
 
+        refreshLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createLocationRequest();
+            }
+        });
+
 
     }
 
@@ -132,21 +139,12 @@ public class WhaleListScreen extends AppCompatActivity  implements GoogleApiClie
             }
         });
 
-        refreshLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createLocationRequest();
-            }
-        });
-
-
         LinearLayout ll = (LinearLayout) findViewById(R.id.whaleLayout);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         lp.setMargins(0, 40, 0, 0);
 
 
         ll.addView(myButton, lp);
-
 
     }
 
