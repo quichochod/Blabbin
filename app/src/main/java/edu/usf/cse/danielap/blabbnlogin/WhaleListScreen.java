712298528346c30a4d256b46495da25f4288342c
Adriano Triana <Adriano Triana>
2017-03-31 00:00:35 -0400

    Fixed the login in so that a user does not have to re login every time they open the app.. unless they are logged out. Also added functionality too add buttons for the whales that are created manually by users.

--- app/src/main/java/edu/usf/cse/danielap/blabbnlogin/WhaleListScreen.java
+++ app/src/main/java/edu/usf/cse/danielap/blabbnlogin/WhaleListScreen.java
@@ -4,6 +4,9 @@ package edu.usf.cse.danielap.blabbnlogin;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -12,6 +15,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class WhaleListScreen extends AppCompatActivity{
@@ -20,7 +25,6 @@ public class WhaleListScreen extends AppCompatActivity{
     ImageButton addWhale;
     EditText inputWhaleName;
     Button submit;
-    ImageButton initialWhale;
 
 
 
@@ -33,7 +37,7 @@ public class WhaleListScreen extends AppCompatActivity{
         addWhale = (ImageButton) findViewById(R.id.buttonAddWhale);
         inputWhaleName = (EditText) findViewById(R.id.NameWhale);
         submit = (Button) findViewById(R.id.SubmitWhaleName);
-        initialWhale = (ImageButton) findViewById(R.id.buttonInitialWhale);
+        //initialWhale = (ImageButton) findViewById(R.id.buttonInitialWhale);
 
         addWhale.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,53 +49,70 @@ public class WhaleListScreen extends AppCompatActivity{
                 submit.setVisibility(view.VISIBLE);
 
 
-
-
             }
         });
 
-        initialWhale.setOnClickListener(new View.OnClickListener(){
+//        initialWhale.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(WhaleListScreen.this, MessageDB.class);
+//                    startActivity(intent);
+//
+//                finish();
+//            }
+//
+//
+//        });
+
+        submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(WhaleListScreen.this, MessageDB.class);
-                    startActivity(intent);
 
-                finish();
+                createNewButton();
+
+
+                addWhale.setVisibility(view.VISIBLE);
+                whaleName.setVisibility(view.VISIBLE);
+
+                inputWhaleName.setVisibility(view.INVISIBLE);
+                submit.setVisibility(view.INVISIBLE);
             }
-
-
         });
-
-
-
 
 
     }
 
 
+    public void createNewButton()
+    {
+        Button myButton = new Button(this);
+        myButton.setText(inputWhaleName.getText().toString());
+        myButton.setTextSize(24);
+        myButton.setBackgroundColor(Color.WHITE);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(WhaleListScreen.this, MessageDB.class);
+                    intent.putExtra("Whale Name", inputWhaleName.getText().toString());
+                    startActivity(intent);
+                //Retrieve whale name like this....
+                //Intent intent = getIntent();
+                //String whaleName = intent.getExtras().getString("Whale Name");
 
-//    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//
-//                if (view.getId() == R.id.buttonAddWhale) {
-//                    View createWhale = WhaleListScreen.this.findViewById(R.id.text_add_whale);
-//                    createWhale.setVisibility(View.INVISIBLE);
-//
-//                    View whaleName = WhaleListScreen.this.findViewById(R.id.NameWhale);
-//                    whaleName.setVisibility(View.VISIBLE);
-//
-//
-//                }
-//
-//
-//            switch (view.getId()) {
-//                case R.id.buttonInitialWhale:
-////                    intent = new Intent(context, BlabbinWhaleActivity.class);
-////                    startActivity(intent);
-//                    break;
-//            }
-//        }
-//    };
+                finish();
+            }
+        });
+
+
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.whaleLayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 40, 0, 0);
+
+
+        ll.addView(myButton, lp);
+
+
+    }
+
 
 }
